e254dd5ddad1d9f437f1c881188e06f4cc4d8aab
yusubhin <sdbtnqlss@gmail.com>
2024-05-11 14:19:03 +0900

    #1 - Spring Data REST 테스트 제외 처리 이것은 이 프로젝트의 비즈니스 로직으로 구현한 내용이 아니라 data rest 기능이고, 통합테스트라 무거우며 db 에도 영향을 준다. 공부 목적으로도 의미가 있기 떄문에 삭제하지는 않고 제외 처리해서 전체 테스트 중에 실행되지 않게끔 처리

--- src/test/java/com/example/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/projectboard/repository/JpaRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.projectboard.repository;
 
 import com.example.projectboard.config.JpaConfig;
 import com.example.projectboard.domain.Article;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 //@ActiveProfiles("testdb") //for spring.sql.init.mode in application.yaml
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class) //DataJpaTest는 JpaConfig 존재를 몰라(내가 만들었거든) 그래서 import로 추가
